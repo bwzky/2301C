72fd09673026a7fa302a0052a61bbb75c3ee6f89
bwzky <2162772469@qq.com>
2023-11-07 15:52:23 +0800

    代码样式

--- 作业/day12-day13/demo/src/views/HomeView.vue
+++ 作业/day12-day13/demo/src/views/HomeView.vue
@@ -1,18 +1,99 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="box">
+      <div class="uimg">
+        <img src="../assets//Snipaste_2023-11-07_10-46-20.png" alt="">
+      </div>
+      <h3>~今天我需要做的事情~</h3>
+      <div class="tian">
+        <input type="text" placeholder="请输入未完成的list">
+        <button>添加</button>
+      </div>
+
+
+      <div class="bun">
+        <p v-for="item,index in list" @click="add(index)" :class="xia==index?'ys':''" >{{ item.name }}</p>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+ data() {
+   return {
+    list:[{name:'3事项'},{name:'查看所有'},{name:'待完成'},{name:'已完成'},{name:'清空所有'}],
+    xia:'',
+   };
+ },
+ methods: {
+  add(index){
+    this.xia=index
+  }
+ },
+ computed: {},
+ filters: {},
+ watch: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.home{
+  background-color: #f2b8b4;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  height: 100vh;
+  .box{
+    height: 600px;
+    width: 300px;
+    border-radius: 5px;
+    margin-top: 50px;
+    background-color: #fff;
+    padding: 10px 30px;
+    
+    .uimg{
+      display: flex;
+      justify-content: center;
+      align-content: center;
+    img{
+      width: 200px;
+      height: 100px;
+    }
+    }
+    h3{
+      text-align: center;
+    }
+    .tian{
+      input{
+        width: 215px;
+        border: none;
+        border-bottom: 3px dashed #ea7a52;
+      }
+      ::-webkit-input-placeholder{
+        color: #ccc;
+      }
+      button{
+        border-radius: 5px;
+        padding: 3px 15px;
+        margin-left: 15px;
+      }
+    }
+    .bun{
+      display: flex;
+      justify-content: space-between;
+      p{
+        font-size: 12px;
+      }
+    }
+    
   }
 }
-</script>
+.ys{
+  background-color: #ea7a52;
+  padding: 2px 10px;
+  color: #fff;
+  border-radius: 5px;
+}
+</style>
